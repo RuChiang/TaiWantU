7bed2f5a7aa419e2caf9a648731294ec3ac3b651
ruchiang <jgchengru@gmail.com>
2018-02-19 09:16:48 +0800

    third commit

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
-var {mongoose} = require('./db/mongoose');
+//checking if the connection is going fine
+try{
+  var {mongoose} = require('./db/mongoose');
+}catch(e){
+  return console.log(e);
+}
 var {demandSchema} = require('./schemas/demandSchema');
 var {donateSchema} = require('./schemas/donateSchema');
 
@@ -10,28 +14,62 @@ var demandModel = mongoose.model('Demand',demandSchema);
 
 
 var app = express();
+var port = process.env.PORT;
+
 app.use(bodyParser.json());
 
 //demand request handling
 app.post('/demand',(req,res)=>{
-  var demandInstance = new demandModel({
-    authority: req.body.authority,
-    member: req.body.member,
+
+  //first search if there is a corresponding donation
+  // Todo:
+  //   1. how to accumulate the amount of the same resource in different entries?
+  //   2. how to split and merge the same resource in different entries?
+  //   3. the JSON data sent back to the user must contain a result field
+  donateModel.findOne({
     category: req.body.category,
     subCategory: req.body.subCategory,
     name: req.body.name,
-    quantity: req.body.quantity,
-    time: req.body.time,
-    location: req.body.location,
-  });
+    quantity: { $gte: req.body.quantity},
+  },'location quality source').then((doc)=>{
+    if(doc == null){
+      //no document is found, save this demand
+      var demandInstance = new demandModel({
+        authority: req.body.authority,
+        member: req.body.member,
+        category: req.body.category,
+        subCategory: req.body.subCategory,
+        name: req.body.name,
+        quantity: req.body.quantity,
+        time: req.body.time,
+        location: req.body.location,
+      });
 
-  demandInstance.save().then((doc)=>{
-    res.send(doc);
-  },(e)=>{
-    res.status(400).send(e);
-  });
+      demandInstance.save().then((doc)=>{
+        res.send({
+          shortResult: 'false',
+          result: 'can\'t find the document now, the demand is saved',
+          doc
+        });
+      },(e)=>{
+        //error upon saving
+        res.status(400).send({
+          shortResult: 'false',
+          e
+        });
+      });
+    }else{
+      //document is found, show it to the user
+      res.send({
+        shortResult: 'true',
+        result: 'document is found',
+        doc
+      });
+    }
+  }).catch((e) => console.log(e));
 })
 
+//donate request handling
 app.post('/donate',(req, res)=>{
   var donateInstance = new donateModel({
     category: req.body.category,
@@ -54,8 +92,8 @@ app.post('/donate',(req, res)=>{
 
 })
 
-app.listen(3000,()=>{
-  console.log('listening on port 3000');
+app.listen(port,()=>{
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = {
